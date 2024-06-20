0400ba4eab77e109db39777509f6008e6edac059
Dialogika <admin@dialogika.co>
2024-06-20 21:02:35 +0700

    bismillah

--- kelas-public-speaking-jogja/assets/js/whatsapp.js
+++ kelas-public-speaking-jogja/assets/js/whatsapp.js
@@ -3,7 +3,7 @@ $(document).on('click','.send_contact', function(){
 
     /* Whatsapp Settings */
     var walink = 'https://web.whatsapp.com/send',
-        phone = '6285780007799',
+        phone = '6285162992597',
         walink2 = 'Salam Hangat, Saya melihat *Website dialogika.co* dan saya tertarik untuk tahu lebih lanjut.' + '%0A%0A' 
                 + 'Sebelumnya perkenalkan saya: ',
         text_yes = 'Terkirim.',
