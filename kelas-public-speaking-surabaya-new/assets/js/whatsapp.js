37e4764749d238fb033f5abfab3ecb6047987167
Yoga Pangestu <yogapangestu16@gmail.com>
2024-10-22 14:33:12 +0700

    fix typo

--- kelas-public-speaking-surabaya-new/assets/js/whatsapp.js
+++ kelas-public-speaking-surabaya-new/assets/js/whatsapp.js
@@ -1,30 +1,3 @@
-$(document).on("click", ".belum-digunakan-nanti-ubah-saja-targetnya", function () {
-  var walink = "https://web.whatsapp.com/send",
-    phone = "6285162992597",
-    defaultMessage =
-      "Salam Hangat, Saya melihat *Website dialogika.co* tentang kursus public speaking online dan saya tertarik untuk tahu lebih lanjut.";
-  (text_yes = "Terkirim."), (text_no = "Isi semua Formulir lalu klik Kirim.");
-
-  if (
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    )
-  ) {
-    walink = "whatsapp://send";
-  }
-
-  // Get custom message dari elementnya
-  var customMessage = $(this).attr("data-message");
-
-  // Final WhatsApp URL
-  var blanter_whatsapp = walink + "?phone=" + phone + "&text=" + encodeURIComponent(defaultMessage + customMessage);
-
-  // Open WA
-  window.open(blanter_whatsapp, "_blank");
-  document.getElementById("text-info").innerHTML =
-    '<span class="yes">' + text_yes + "</span>";
-});
-
 $(document).on("click", ".send_contact", function () {
   var input_blanter = document.getElementById("names");
 
@@ -87,4 +60,31 @@ $(document).on("click", ".send_contact", function () {
     document.getElementById("text-info").innerHTML =
       '<span class="no">' + text_no + "</span>";
   }
+});
+
+$(document).on("click", ".belum-digunakan-nanti-ubah-saja-targetnya", function () {
+  var walink = "https://web.whatsapp.com/send",
+    phone = "6285162992597",
+    defaultMessage =
+      "Salam Hangat, Saya melihat *Website dialogika.co* tentang kursus public speaking online dan saya tertarik untuk tahu lebih lanjut.";
+  (text_yes = "Terkirim."), (text_no = "Isi semua Formulir lalu klik Kirim.");
+
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    walink = "whatsapp://send";
+  }
+
+  // Get custom message dari elementnya
+  var customMessage = $(this).attr("data-message");
+
+  // Final WhatsApp URL
+  var blanter_whatsapp = walink + "?phone=" + phone + "&text=" + encodeURIComponent(defaultMessage + customMessage);
+
+  // Open WA
+  window.open(blanter_whatsapp, "_blank");
+  document.getElementById("text-info").innerHTML =
+    '<span class="yes">' + text_yes + "</span>";
 });
