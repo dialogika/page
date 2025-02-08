b0fd7536e456eb6d99d6004cdf8a4dcee43b5ae4
Yoga Pangestu <yogapangestu16@gmail.com>
2025-02-08 11:11:48 +0700

    update api

--- kelas-public-speaking-surabaya-new/assets/js/clickup.js
+++ kelas-public-speaking-surabaya-new/assets/js/clickup.js
@@ -9,7 +9,7 @@ document
     const address = document.getElementById("locations").value;
 
     // API Token dan List ID ClickUp
-    const apiToken = "pk_3640079_B56O8X0HW6FAEIZJFFJAQW99IAHQMF8N";
+    const apiToken = "pk_276677813_5LZTC2L1TYHRVBRRRK5BKXBZDVUU2X7E";
     const listId = "14355106";
     const whatsappUrl = `https://wa.me/62${whatsapp}`;
 
